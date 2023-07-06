b04a9acb012f8512d60f3cd0ef5a43dc3a0ddf36
이효진 <director20844@gmail.com>
2023-07-06 13:47:35 +0900

    fix: index.css, GuestbookAll

---
+++ src/components/Header.style.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const HeaderIcon = styled.div``
+
+export const HeaderText = styled.div`
+    text-align: center;
+`
+
+export const HeaderBody = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    border: black solid 1px; //테스트용
+`
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import * as Style from './Header.style'
+
+function Header() {
+    return <Style.HeaderBody>
+        <Style.HeaderIcon>뒤로가기</Style.HeaderIcon>
+        <Style.HeaderIcon>뒤로가기</Style.HeaderIcon>
+        <Style.HeaderIcon>뒤로가기</Style.HeaderIcon>
+    </Style.HeaderBody>
+}
+
+export default Header
