b9e0e7b153d02cf7a7bbc7d2c4f2e8f930491e58
steven-yn <ssungyeon94@gmail.com>
2023-07-06 14:22:30 +0900

    add: 파일 인풋 컴포넌트 버튼 추가

---
+++ src/components/AddPost/AddPost.style.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const AddPostInput = styled.input`
+  position: fixed;
+  bottom: 0.9375rem;
+  right: 0.875rem;
+
+  width: 3rem;
+  height: 3rem;
+
+  background-color: #ccc;
+`;
+
+export default AddPostInput;
---
+++ src/components/AddPost/AddPost.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import AddPostInput from './AddPost.style';
+import usePostStore from 'lib/hook/store/usePostStore';
+
+const AddPost = () => {
+  const { imageUrl, setImageFile, setImageUrl } = usePostStore();
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const imgFile = e && e.target.files && e.target.files[0];
+
+    if (!e.target.files || !imgFile) {
+      console.error('이미지 파일이 감지되지 않음');
+      return undefined;
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(imgFile);
+
+    reader.onload = () => {
+      setImageUrl(reader.result as string);
+      setImageFile(imgFile);
+    };
+  };
+
+  return (
+    <AddPostInput
+      type="file"
+      accept="image/*"
+      value={imageUrl}
+      onChange={onChange}
+    ></AddPostInput>
+  );
+};
+
+export default AddPost;
