ede88baf2e7952a3cb93eb28e7481bdec3b87259
이효진 <director20844@gmail.com>
2023-07-07 04:29:04 +0900

    fix: map Bottom Btn

--- src/pages/MapPage.style.tsx
+++ src/pages/MapPage.style.tsx
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
+export const MapPageBottom = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  width: inherit;
+`
+
 export const MapPageDiv = styled.div`
   width: 100vw;
   height: 100vh;
--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import KakaoMap from '../components/KakaoMap/KakaoMap';
-import { MapPageDiv } from './MapPage.style';
-import SubHeader from '../components/Header/SubHeader';
-import SubFooter from '../components/Footer/SubFooter';
+import React from "react";
+import KakaoMap from "../components/KakaoMap/KakaoMap";
+import { MapPageDiv, MapPageBottom } from "./MapPage.style";
+import SubHeader from "../components/Header/SubHeader";
+// import SubFooter from '../components/Footer/SubFooter';
+import BottomBtn from "../components/BottomBtn/BottomBtn";
+import { BtnProps } from "../types/BtnProps";
+import { useNavigate } from "react-router-dom";
 
 const MapPage = () => {
+  const navigate = useNavigate();
+
+  const goGuestbook: BtnProps = {
+    content: "방명록 작성하기",
+    handler: () => {
+      navigate('/addpostlocation')
+    },
+  };
+
   return (
     <MapPageDiv>
       <SubHeader />
       <KakaoMap />
-      <SubFooter />
+      <MapPageBottom>
+        <BottomBtn {...goGuestbook} />
+      </MapPageBottom>
     </MapPageDiv>
   );
 };
