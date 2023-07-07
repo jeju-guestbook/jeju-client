1c9d15e561d727be4d28b693fb4ccd1e1fa5a156
steven-yn <ssungyeon94@gmail.com>
2023-07-07 10:51:36 +0900

    줄은 무시되고, 메시지를 입력하지 않으면 커밋이 중지됩니다.

--- src/components/CreateCard/CreateCardLayout.tsx
+++ src/components/CreateCard/CreateCardLayout.tsx
@@ -1,14 +1,14 @@
-import CreateCardFrame from "../icons/CreateCardFrame";
-import TestImg from "../../public/Test/KakaoTalk_20230707_102642558.jpg";
+import CreateCardFrame from '../icons/CreateCardFrame';
+import TestImg from '../../public/Test/KakaoTalk_20230707_102642558.jpg';
 
-import * as Style from "./CreateCardLayout.style";
+import * as Style from './CreateCardLayout.style';
 
 function CreateCardLayout() {
   return (
     <Style.CreateCardContainer>
       <CreateCardFrame />
-      <img src={TestImg} alt={"TEST img"} />
-      <img src={TestImg} alt={"TEST img"} />
+      <img src={TestImg} />
+      <img src={TestImg} />
     </Style.CreateCardContainer>
   );
 }
--- src/pages/Submitted.tsx
+++ src/pages/Submitted.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { useState } from 'react';
 
 import * as Style from './Submitted.style';
@@ -48,7 +49,31 @@ function Submitted() {
               });
             }}
           />
-          <img src={KakaoIcon} alt="kakao Sharing" />
+          {/* <img src="/test.jpeg"></img> */}
+          <img
+            src={KakaoIcon}
+            alt="kakao Sharing"
+            onClick={() => {
+              if (window.Kakao) {
+                window.Kakao.Share.createDefaultButton({
+                  container: '#kakaotalk-sharing-btn',
+                  objectType: 'feed',
+                  content: {
+                    title: document.title,
+                    description: `제주도의 푸르른 풍경 속에서 눈에 띄는 감귤 나무들이 햇볕에 반짝이며
+                    농장을 수놓습니다. 나무 가지에 매달린 풍성한 감귤들은 탱글탱글하게 ...`,
+                    imageUrl: `${window.location.origin}/test.jpeg`,
+                    link: {
+                      mobileWebUrl:
+                        'https://creative-empanada-b8c6f1.netlify.app/createcard/1',
+                      webUrl:
+                        'https://creative-empanada-b8c6f1.netlify.app/createcard/1',
+                    },
+                  },
+                });
+              }
+            }}
+          />
         </Style.SharingBtnGroup>
       </div>
 
