23308a4781b081cddffebd529296549fe1cd93a5
steven-yn <ssungyeon94@gmail.com>
2023-07-07 10:58:51 +0900

    fix

--- src/components/BottomBtn/BottomBtn.style.tsx
+++ src/components/BottomBtn/BottomBtn.style.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const BottomBtnBody = styled.div`
   display: flex;
@@ -6,17 +6,16 @@ export const BottomBtnBody = styled.div`
   align-items: center;
 
   position: absolute;
-  bottom: 1.375rem;
+  bottom: -32px;
 
   width: 20.375rem;
   height: 3.75rem;
 
   border-radius: 5px;
-  border: 1px solid #6002ee;
   background: rgba(96, 2, 238, 0.1);
 
   color: #6002ee;
-  font-family: "Pretendard-SemiBold";
+  font-family: 'Pretendard-SemiBold';
   font-size: 1.125rem;
   line-height: 1.625rem;
   letter-spacing: -0.0281rem;
--- src/pages/MapPage.style.tsx
+++ src/pages/MapPage.style.tsx
@@ -1,14 +1,20 @@
 import styled from 'styled-components';
 
 export const MapPageBottom = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
-  
-  width: inherit;
-`
+
+  height: 40px;
+
+  padding-top: 0.9375rem;
+`;
 
 export const MapPageDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+
   width: 100vw;
   height: 100vh;
 `;
