8b698149f2b104b681a6b8002166ebd470b795ee
steven-yn <ssungyeon94@gmail.com>
2023-07-17 22:35:20 +0900

    fix:플롯 버튼 동작 수정

--- src/components/AddPost/AddPostFloatBtn.tsx
+++ src/components/AddPost/AddPostFloatBtn.tsx
@@ -6,43 +6,47 @@ import resizeImage from '../../lib/modules/resizeImage';
 import initValue from '../../lib/const/initValue';
 
 const AddPostFloatBtn = () => {
-  const { imageUrl, imageFile, setImageFile, setImageUrl } = usePostStore();
+  // const { imageUrl, imageFile, setImageFile, setImageUrl } = usePostStore();
   const inputRef = React.useRef<HTMLInputElement>(null);
   const navigate = useNavigate();
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const imgFile = e && e.target.files && e.target.files[0];
+  // const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const imgFile = e && e.target.files && e.target.files[0];
 
-    console.log(e.target.files, 'e.target.files');
+  //   console.log(e.target.files, 'e.target.files');
 
-    if (!e.target.files || !imgFile) {
-      console.error('이미지 파일이 감지되지 않음');
-      return undefined;
-    }
+  //   if (!e.target.files || !imgFile) {
+  //     console.error('이미지 파일이 감지되지 않음');
+  //     return undefined;
+  //   }
 
-    const reader = new FileReader();
-    reader.readAsDataURL(imgFile);
+  //   const reader = new FileReader();
+  //   reader.readAsDataURL(imgFile);
 
-    reader.onload = async (e) => {
-      const { resizedDataUrl, resizedFile } = await resizeImage({
-        src: e.target?.result as string,
-        name: imgFile.name,
-        type: imgFile.type,
-        lastModified: imgFile.lastModified,
-        maxWidth: initValue.imgSize.x,
-        maxHeight: initValue.imgSize.y,
-      });
+  //   reader.onload = async (e) => {
+  //     const { resizedDataUrl, resizedFile } = await resizeImage({
+  //       src: e.target?.result as string,
+  //       name: imgFile.name,
+  //       type: imgFile.type,
+  //       lastModified: imgFile.lastModified,
+  //       maxWidth: initValue.imgSize.x,
+  //       maxHeight: initValue.imgSize.y,
+  //     });
 
-      setImageUrl(resizedDataUrl);
-      setImageFile(resizedFile);
-    };
+  //     setImageUrl(resizedDataUrl);
+  //     setImageFile(resizedFile);
+  //   };
+  // };
+
+  // useEffect(() => {
+  //   if (imageFile && imageUrl) {
+  //     navigate('/addpostlocation');
+  //   }
+  // }, [imageUrl, imageFile]);
+
+  const onClick = () => {
+    navigate('/addpostlocation');
   };
-
-  useEffect(() => {
-    if (imageFile && imageUrl) {
-      navigate('/addpostlocation');
-    }
-  }, [imageUrl, imageFile]);
 
   return (
     <AddPostLabel>
@@ -50,7 +54,7 @@ const AddPostFloatBtn = () => {
         ref={inputRef}
         type="file"
         accept="image/*"
-        onChange={onChange}
+        onClick={onClick}
       />
     </AddPostLabel>
   );
